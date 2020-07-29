5b7bfb374d5d053f84039268f52980d65d74a8be
guidijie <2316288589@qq.com>
2020-07-29 09:22:29 +0800

    查询订单模块-----02

---
+++ src/main/java/com/jie/stupiddog/vo/TimeAndStateVo.java
@@ -0,0 +1,14 @@
+package com.jie.stupiddog.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimeAndStateVo {
+    private String time;
+    private String state;
+}
